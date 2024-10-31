4f4f6bd91cadb475607b33289215590420f31f5e
Clément <clemmoller@gmail.com>
2024-10-31 16:54:16 +0100

    feat: add Base and Shared tests

---
+++ test/Base.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.27;
+
+// Foundry
+import {Test} from "forge-std/Test.sol";
+
+// Contracts
+import {OETH} from "origin/token/OETH.sol";
+import {OETHProxy} from "origin/proxies/Proxies.sol";
+
+// Interfaces
+import {IVault} from "origin/interfaces/IVault.sol";
+
+abstract contract Base_Test_ is Test {
+    //////////////////////////////////////////////////////
+    /// --- CONTRACTS & INTERFACES
+    //////////////////////////////////////////////////////
+    // --- Contracts ---
+    OETH public oeth;
+    OETHProxy public oethProxy;
+
+    // --- Interfaces ---
+    IVault public vault;
+
+    //////////////////////////////////////////////////////
+    /// --- GOVERNANCE, MS & EOAS
+    //////////////////////////////////////////////////////
+    // --- EOAs ---
+    address public alice;
+    address public bob;
+    address public charlie;
+    address public dave;
+    address public eve;
+    address public frank;
+    
+    // --- Governance ---
+    address public deployer;
+    address public governor;
+
+    //////////////////////////////////////////////////////
+    /// --- DEFAULT VALUES
+    //////////////////////////////////////////////////////
+    uint256 public constant DEFAULT_AMOUNT = 1 ether;
+
+    //////////////////////////////////////////////////////
+    /// --- SETUP
+    //////////////////////////////////////////////////////
+    function setUp() public virtual {}
+
+    /// @notice Better if called once all contract have been depoyed.
+    function _labelAll() internal virtual {
+        // Contracts
+        vm.label(address(oeth), "OETH");
+        vm.label(address(oethProxy), "OETH Proxy");
+        vm.label(address(vault), "OETH Vault");
+
+        // Governance, multisig and EOAs
+        _labelNotNull(alice, "Alice");
+        _labelNotNull(bob, "Bob");
+        _labelNotNull(charlie, "Charlie");
+        _labelNotNull(dave, "Dave");
+        _labelNotNull(eve, "Eve");
+        _labelNotNull(frank, "Frank");
+
+        _labelNotNull(deployer, "Deployer");
+        _labelNotNull(governor, "Governor");
+    }
+
+    function _labelNotNull(address _address, string memory _name) internal virtual {
+        if (_address != address(0)) vm.label(_address, _name);
+    }
+}
---
+++ test/Shared.sol
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.27;
+
+// Test imports
+import {Base_Test_} from "test/Base.sol";
+
+// Contracts
+import {OETH} from "origin/token/OETH.sol";
+import {OETHProxy} from "origin/proxies/Proxies.sol";
+
+// Interfaces
+import {IVault} from "origin/interfaces/IVault.sol";
+
+abstract contract Shared_Test_ is Base_Test_ {
+    //////////////////////////////////////////////////////
+    /// --- STORAGE
+    //////////////////////////////////////////////////////
+    address[] public users;
+
+    //////////////////////////////////////////////////////
+    /// --- SETUP
+    //////////////////////////////////////////////////////
+    function setUp() public virtual override {
+        // 1. Setup a realistic test environnement.
+        _setUpRealisticEnvironnement();
+
+        // 2. Create user.
+        _createUsers();
+
+        // 3. Deploy mocks. We don't have mocks at the moment.
+        // _deployMocks();
+
+        // 4. Deploy contracts.
+        _deployContracts();
+
+        // 5. Label addresses.
+        _labelAll();
+    }
+
+    function _setUpRealisticEnvironnement() private {
+        vm.roll(21000000); // block number
+        vm.warp(1730419200); // timestamp
+    }
+
+    function _createUsers() private {
+        // Users with role
+        deployer = makeAddr("Deployer");
+        governor = makeAddr("Governor");
+        vault = IVault(makeAddr("Vault"));
+
+        // Random users
+        alice = makeAddr("Alice");
+        bob = makeAddr("Bob");
+        charlie = makeAddr("Charlie");
+        dave = makeAddr("Dave");
+        eve = makeAddr("Eve");
+        frank = makeAddr("Frank");
+
+        // Add users to the list
+        users.push(alice);
+        users.push(bob);
+        users.push(charlie);
+        users.push(dave);
+        users.push(eve);
+        users.push(frank);
+    }
+
+    function _deployContracts() private {
+        vm.startPrank(deployer);
+
+        // 1. Deploy proxy.
+        oethProxy = new OETHProxy();
+
+        // 2. Deploy OETH implementation.
+        oeth = new OETH();
+
+        // 3. Initialize proxy.
+        bytes memory data = abi.encodeWithSignature(
+            "initialize(string,string,address,uint256)", "Origin ETH", "OETH", address(vault), 1e27
+        );
+
+        oethProxy.initialize(address(oeth), governor, data);
+
+        // 4. Set proxy as OETH.
+        oeth = OETH(address(oethProxy));
+
+        vm.stopPrank();
+    }
+}
